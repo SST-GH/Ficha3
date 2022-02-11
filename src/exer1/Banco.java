92963ac76fb5fdd0a78538e748d61119b1bcaa72
studentsara <97111715+studentsara@users.noreply.github.com>
2022-02-11 11:02:47 +0000

    s

--- src/exer1/Banco.java
+++ src/exer1/Banco.java
@@ -1,5 +1,11 @@
 package exer1;
 
+import java.util.List;
+
 public class Banco {
+	
+	private String nome;
+	private List<ContaBancaria>contas;
+	
 
 }
--- src/exer1/Main.java
+++ src/exer1/Main.java
@@ -1,6 +1,8 @@
 package exer1;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Main {
 	
@@ -11,6 +13,14 @@ public class Main {
 		ContaBancaria conta3 = new ContaBancaria("Jojo", new Date());
 		ContaBancaria conta4 = new ContaBancaria("Sara", 5000);
 		
+		List<ContaBancaria>contas = new ArrayList<ContaBancaria>();
+		
+		contas.add(conta1);
+		contas.add(conta2);
+		contas.add(conta3);
+		contas.add(conta4);
+		
+		
 		conta4.depositar(10_000);
 		conta4.levantar(1000);
 		System.out.println(conta4.getInformacaoConta() + "\n");
@@ -19,6 +29,8 @@ public class Main {
 		conta3.depositar(1500);
 		conta3.levantar(10000);
 		System.out.println(conta3.getInformacaoConta());
+		
+		
 		
 		
 		
