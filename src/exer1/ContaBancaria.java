da6817bba272f361b4a1729cc79b95fc6173e52a
studentsara <97111715+studentsara@users.noreply.github.com>
2022-02-27 20:37:37 +0000

    s

--- src/exer1/ContaBancaria.java
+++ src/exer1/ContaBancaria.java
@@ -72,4 +72,6 @@ public class ContaBancaria{
 	public Date dataAbertura() {
 		return dataAbertura;
 	}
+	
+	
 }
--- src/exer1/Main.java
+++ src/exer1/Main.java
@@ -42,21 +42,25 @@ public class Main {
 		conta6.levantar(700);
 
 		var casa1 = new Casa("Rua do Funchal");
+		banco.adicionarCasa(casa1);
 		casa1.setPrecoCusto(1_500_000);
 		casa1.setPrecoVenda(2_000_000);
 		
-		System.out.println("\nA casa vendida na morada " + casa1.getMorada() + " tem uma margem de lucro de "+ casa1.getMargemLucro() + "€" +
-		", essa margem em termos percentuais é de " + casa1.getPercentMargemLucro() + "%");
+		System.out.println("\nA casa vendida na morada " + casa1.getMorada() + " tem uma margem de lucro de " + 
+		casa1.getMargemLucro() + "€" + ", essa margem em termos percentuais é de " + casa1.getPercentMargemLucro() + "%");
 		
 		var casa2 = new Casa("Avenida da Liberdade");
+		banco.adicionarCasa(casa2);
 		casa2.setPrecoCusto(700_000);
 		casa2.setPrecoVenda(500_000);
-		System.out.println("\nA casa vendida na morada " + casa2.getMorada() + " tem uma percentagem de margem de lucro de " + casa2.getPercentMargemLucro() + "%");
+		System.out.println("\nA casa vendida na morada " + casa2.getMorada() + " tem uma percentagem de margem de lucro de " +
+		casa2.getPercentMargemLucro() + "%");
 	
 		var casa3 = new Casa("Caminho do Porto");
-		casa3.setPrecoCusto(200_000);
-		casa3.setPrecoVenda(300_000);
-		System.out.println("\nA casa vendida na morada " + casa3.getMorada() + " tem uma percentagem de margem de lucro de " + casa3.getPercentMargemLucro() + "%");
-	
+		banco.adicionarCasa(casa3);
+		
+		
+		banco.retirarCasa("Avenida da Liberdade");
+		
 	}
 }
